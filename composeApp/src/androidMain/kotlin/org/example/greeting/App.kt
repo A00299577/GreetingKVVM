e920488ef601cc0c0e6336343fd8a7d2d095f69e
murali-ganta <muralireddy2910@gmail.com>
2025-01-29 00:00:34 -0500

    added Greeting file and made changes in App.kt file

--- composeApp/src/androidMain/kotlin/org/example/greeting/App.kt
+++ composeApp/src/androidMain/kotlin/org/example/greeting/App.kt
@@ -1,36 +1,28 @@
 package org.example.greeting
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
+
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import greetingkvvm.composeapp.generated.resources.Res
-import greetingkvvm.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+        Column(modifier = Modifier.padding(12.dp)) {
+            Greeting().greet().forEach { courseCode ->
+                Text(
+                    text = courseCode,
+                    modifier = Modifier.blur(1.dp),
+                    fontWeight = FontWeight.Bold
+                )
             }
         }
     }
---
+++ composeApp/src/androidMain/kotlin/org/example/greeting/Greeting.kt
@@ -0,0 +1,7 @@
+package org.example.greeting
+
+class Greeting {
+    fun greet(): List<String> {
+        return  listOf("STY6510 - 13205", "STY4040 - 13206", "ISP1969 - 13208", "CMP5555 - 13207")
+    }
+}
